6a4de796f45e3b31cab041f64f6342552659d08b
ranebhushan <bhushanrane13@gmail.com>
2021-12-02 11:06:05 -0500

    Created basic structure of server node

--- scripts/rrp_ik_server.py
+++ scripts/rrp_ik_server.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python
 
+from rbe500_project.srv import rrpIK
 import rospy
 import time
+
+def callback_function(request):
+    # Implement Inverse Kinematics here
+    return 1
+
+def rrp_ik_server():
+    rospy.init_node("RRP_IK_service")
+    service = rospy.Service("rrpIK", rrpIK, callback_function)
+    rospy.spin()
+
+if __name__ == "__main__":
+    rrp_ik_server()
