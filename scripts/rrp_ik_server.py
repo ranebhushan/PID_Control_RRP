03153c804076206a6239bdf6ebe33e592c5444ae
ranebhushan <bhushanrane13@gmail.com>
2021-12-02 22:26:48 -0500

    Added table of DH paramters for RRP robot

--- scripts/rrp_ik_server.py
+++ scripts/rrp_ik_server.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python
 
-from rbe500_project.srv import rrpIK
+from rbe500_project.srv import rrpIK, rrpIKResponse
+
 import rospy
 import time
 
+# |------------------------------------------------------|
+# |----------------TABLE OF DH PARAMETERS----------------|
+# |------------------------------------------------------|
+# |  LINK    |   THETA   |   d       |   a   |   ALPHA   |
+# |   1      |   theta1  |   L1      |   L2  |   0       | 
+# |   2      |   theta2  |   0       |   L3  |   pi      |
+# |   3      |    0      |   d3+L4   |   0   |   0       |
+# |------------------------------------------------------|
+
 def callback_function(request):
     # Implement Inverse Kinematics here
+    #joint1 = atan2(request.x_position, request.y_position)
     return 1
 
 def rrp_ik_server():
