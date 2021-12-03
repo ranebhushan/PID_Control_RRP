6bd51bea509039868ac44c6947b3702f314c1185
ranebhushan <bhushanrane13@gmail.com>
2021-12-03 16:09:33 -0500

    ROS Topic Subscription Unsuccessful

--- scripts/test_subscriber.py
+++ scripts/test_subscriber.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
-
+import time
 from sensor_msgs.msg import JointState
 
 class Test_Sub():
@@ -9,18 +9,18 @@ class Test_Sub():
     def __init__(self):
         # ROS
         # subscribe to keyboard input
-        self.current_value = JointState()
+        
         self.joint_state_subscriber = rospy.Subscriber("/rrp/joint_states", JointState, self.joint_state_callback)
        
     def joint_state_callback(self, data):
-
+        
+        self.current_value = JointState()
         print(self.current_value)
         # print("Current Position :", self.current_value.position)
         # print("Current Velocity :", self.current_value.velocity) 
 
 if __name__ == "__main__":
-    # Launch ros node class
-    rospy.init_node('sub_test_node')
+    rospy.init_node('test_sub')
     sub = Test_Sub()
 
     # Set rate and run
