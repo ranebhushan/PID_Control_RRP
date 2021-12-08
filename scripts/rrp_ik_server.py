9612e1af502c0be9aefe7df898dcc887023564bc
ranebhushan <bhushanrane13@gmail.com>
2021-12-08 18:53:49 -0500

    Added IK for joint 1

--- scripts/rrp_ik_server.py
+++ scripts/rrp_ik_server.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
-from rbe500_project.srv import rrpIK, rrpIKResponse
+from rbe500_project.srv import rrpIK
 
 import rospy
 import time
+import math
+import numpy
 
 # |------------------------------------------------------|
 # |----------------TABLE OF DH PARAMETERS----------------|
@@ -15,13 +17,21 @@ import time
 # |------------------------------------------------------|
 
 def callback_response(request):
-    # Implement RRP ROBOT Inverse Kinematics here
-    #joint1 = atan2(request.x_position, request.y_position)
-    return ("1+ and OnePlus")
+    # Implement RRP Robot Inverse Kinematics here
+    rospy.loginfo("Input Position: X = " + str(request.x_position) + " Y = " + str(request.y_position) + " Z = " + str(request.z_position))
+    joint1 = math.atan2(request.y_position, request.x_position)
+  
+    # Testing Purpose
+    joint2 = request.y_position + 2 
+    joint3 = request.z_position + 3
+    rospy.loginfo("Joint Angles (in radians) : theta1 = " + str(joint1) + " theta2 = " + str(joint2) + " d3 = " + str(joint3))
+    rospy.loginfo("Joint Angles (in degrees ) : theta1 = " + str(numpy.rad2deg(joint1)) + " theta2 = " + str(numpy.rad2deg(joint2)) + " d3 = " + str(joint3))
+    return (joint1, joint2, joint3)
 
 def rrp_ik_server():
     rospy.init_node("RRP_IK_service")
     service = rospy.Service("rrpIK", rrpIK, callback_response)
+    rospy.loginfo("Ready to solve the Inverse Kinematics")
     rospy.spin()
 
 if __name__ == "__main__":
